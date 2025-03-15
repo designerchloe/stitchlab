6b7f9c25ad4caadd49f688622e22ce4e815f4ba7
designerChloe <chloe.allard72@gmail.com>
2025-03-15 14:14:04 -0400

    Style page & title container

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,11 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <h1>StitchLab</h1>
-      <h2>Build your very own knit & crochet tapestry patterns using pre-built mini motifs.</h2>
+    <div className="page-container">
+      <div className="title-container">
+        <h1>StitchLab</h1>
+        <h2>Build your very own knit & crochet tapestry patterns using pre-built mini motifs.</h2>
+      </div>
     </div>
   )
 }
